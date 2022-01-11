4caea5756430328f8a6b374885a2c291dd4f076e
Ignacio <juan.delaiglesia.jdli@gmail.com>
2022-01-11 16:09:36 -0300

    rollback test changes

--- src/main/java/ideas/pizzeriaRemolo/PizzeriaRemoloApplication.java
+++ src/main/java/ideas/pizzeriaRemolo/PizzeriaRemoloApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzeriaRemoloApplication {
-	//test escribir desde git.dev
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaRemoloApplication.class, args);
 	}
